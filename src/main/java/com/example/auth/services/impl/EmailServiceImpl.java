5c8462feab77f3f75144d698c2b11a0742be77eb
italo renan <italorenan225@gmail.com>
2023-08-14 18:17:58 -0300

    feat: create implementation to EmailService

---
+++ src/main/java/com/example/auth/services/impl/EmailServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.auth.services.impl;
+
+import com.example.auth.services.EmailService;
+
+public class EmailServiceImpl implements EmailService {
+
+    @Override
+    public void sendMailMessage(String name, String to, String token) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sendMailMessage'");
+    }
+
+}
