b0f0c1ba3c6e2c5ce2c17473f4f7a31af90d01b7
italo renan <italorenan225@gmail.com>
2023-08-17 09:57:49 -0300

    refactor: correct name of variable

--- src/main/java/com/example/auth/services/impl/TokenServiceImpl.java
+++ src/main/java/com/example/auth/services/impl/TokenServiceImpl.java
@@ -1,23 +1,22 @@
 package com.example.auth.services.impl;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.auth.entities.User;
 import com.example.auth.services.TokenService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenServiceImpl implements TokenService {
 
-    @Value("${api.security.toke.secret}")
+    @Value("${api.security.token.secret}")
     private String secret;
 
     public String generateToken(User user) {
