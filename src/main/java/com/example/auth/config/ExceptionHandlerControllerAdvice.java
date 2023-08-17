bdb4c409678040157d48da720b7ce9358251026d
italo renan <italorenan225@gmail.com>
2023-08-17 19:35:09 -0300

    feat: add custom exception to validations

--- src/main/java/com/example/auth/config/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/example/auth/config/ExceptionHandlerControllerAdvice.java
@@ -5,11 +5,14 @@ import com.example.auth.expections.EntityInvalidException;
 import com.example.auth.expections.StandardException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.Objects;
 
 @ControllerAdvice
 public class ExceptionHandlerControllerAdvice {
@@ -33,5 +36,17 @@ public class ExceptionHandlerControllerAdvice {
                 "Authentication failed at controller advice",
                 ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(re);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseBody
+    public ResponseEntity<StandardException> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        Object message = Arrays.stream(Objects.requireNonNull(ex.getDetailMessageArguments())).toArray()[1];
+        StandardException re = new StandardException(
+                Instant.now(),
+                HttpStatus.UNAUTHORIZED.value(),
+                "method argument not valid",
+                message.toString());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(re);
     }
 }
