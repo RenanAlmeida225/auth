55986113c9f03f1690fdfbce0f5738d25c1e0204
italo renan <italorenan225@gmail.com>
2023-08-17 09:59:10 -0300

    refactor: remove variable token and return function

--- src/main/java/com/example/auth/services/impl/TokenServiceImpl.java
+++ src/main/java/com/example/auth/services/impl/TokenServiceImpl.java
@@ -22,12 +22,12 @@ public class TokenServiceImpl implements TokenService {
     public String generateToken(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            String token = JWT.create()
+            return JWT.create()
                     .withIssuer("auth-api")
                     .withSubject(user.getEmail())
                     .withExpiresAt(genExpirationDate())
                     .sign(algorithm);
-            return token;
+
         } catch (JWTCreationException e) {
             throw new RuntimeException("Error while generation token", e);
         }
